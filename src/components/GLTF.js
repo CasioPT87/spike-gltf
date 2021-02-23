dd4ab5f7dbfda18cb1320fa0845bfc9e200e6df8
Sesko <sergioibanezmoreno@gmail.com>
2021-02-23 14:59:18 +0000

    A little bit of refactoring

--- src/components/GLTF.js
+++ src/components/GLTF.js
@@ -3,7 +3,6 @@ import * as THREE from "three";
 export default class GLTF extends React.Component {
 
   componentDidMount() {
-    // === THREE.JS CODE START ===
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
     var renderer = new THREE.WebGLRenderer();
@@ -14,14 +13,14 @@ export default class GLTF extends React.Component {
     var cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
     camera.position.z = 5;
-    var animate = function () {
-      requestAnimationFrame( animate );
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      renderer.render( scene, camera );
-    };
-    animate();
-    // === THREE.JS EXAMPLE CODE END ===
+    this.animate(scene, camera, renderer, cube);
+  }
+
+  animate = (scene, camera, renderer, cube) => {
+    requestAnimationFrame(() => this.animate(scene, camera, renderer, cube) );
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render( scene, camera );
   }
   
   render() {
